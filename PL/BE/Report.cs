b72c109393d71f9ec61f1212881a91410c3a6e21
אשר אלכסנדר <asherb94@gmail.com>
2018-12-13 11:50:25 +0200

    started

---
+++ PL/BE/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Report
+    {
+        DateTime timeOfReport { get; set; }
+        string name { get; set; }
+        string address { get; set; } // Location
+        int numOfBombs { get; set; }
+    }
+}
