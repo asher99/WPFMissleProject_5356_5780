622f99752f64eb6bcc3c240b989e0157d435c176
chagay levi <chagay88@gmail.com>
2019-01-10 11:33:00 +0200

    entitis and DB

---
+++ PL/BE/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Event
+    {
+        public int IDOfEvent { set; get; }
+        public DateTime beginningOfEvent { set; get; }
+        public ICollection<Report> listOfReport { set; get; }
+        public ICollection<Hit> listOfHit { set; get; }
+    }
+}
---
+++ PL/BE/Hit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Device.Location;
+
+namespace BE
+{
+    public class Hit
+    {
+        public DateTime timeOfHit { set; get; }
+        public GeoCoordinate address { set; get; } // Location
+        public Image locationImage { set; get; }
+        public int EventID { get; set; }
+    }
+}
