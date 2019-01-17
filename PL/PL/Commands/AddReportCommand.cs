6738c4df23fe0d661770b9d360e06bb89c766416
chagay levi <chagay88@gmail.com>
2019-01-17 13:48:54 +0200

    mvvm add report ui

---
+++ PL/PL/Commands/AddReportCommand.cs
@@ -0,0 +1,40 @@
+﻿using PL.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PL.Commands
+{
+    public class AddReportCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private IReportViewModel CurrentVM;
+
+        public bool CanExecute(object parameter)
+        {
+            bool result = true;
+            var values = (object[])parameter;
+            foreach (var item in values)
+            {
+                if (item == null)
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        public void Execute(object parameter)
+        {
+
+        }
+    }
+}
