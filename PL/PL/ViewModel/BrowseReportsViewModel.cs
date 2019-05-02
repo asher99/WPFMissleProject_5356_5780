b8dfaa5e9089eb968d1b8b25c28bea59f6028445
asher alexander <asherb94@gmail.com>
2019-05-02 13:33:26 +0300

    pie chart

---
+++ PL/PL/ViewModel/BrowseReportsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using PL.Model;
+
+namespace PL.ViewModel
+{
+    public class BrowseReportsViewModel
+    {
+        public MissleModel currentModel { set; get; }
+
+        public int North = 3;
+
+        public List<Report> getAllReports
+        {
+            set { }
+            get
+            {            
+                  return currentModel.allReports();                
+            }
+        }
+    }
+}
