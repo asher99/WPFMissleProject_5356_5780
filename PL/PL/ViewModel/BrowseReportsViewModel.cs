5d8d4c6c898e2481375cb8d3b53b5fcd7d3d7137
chagay levi <chagay88@gmail.com>
2019-05-05 22:59:57 +0300

    browse

--- PL/PL/ViewModel/BrowseReportsViewModel.cs
+++ PL/PL/ViewModel/BrowseReportsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,16 +12,44 @@ namespace PL.ViewModel
     public class BrowseReportsViewModel
     {
         public MissleModel currentModel { set; get; }
+        public ObservableCollection<Report> North { get; set; }
+        public ObservableCollection<Report> South { get; set; }
+        public ObservableCollection<Report> Center { get; set; }
+        public ObservableCollection<Report> Other { get; set; }
 
-        public int North { set; get; }
+        //public int North { set; get; }
 
-        public List<Report> getAllReports
+        public BrowseReportsViewModel()
         {
-            set { }
-            get
+            North = new ObservableCollection<Report>();
+            South = new ObservableCollection<Report>();
+            Center = new ObservableCollection<Report>();
+            Other = new ObservableCollection<Report>();
+        }
+
+        public async void getAllReports()
+        {
+            var allreport = await currentModel.allReports();
+            foreach (var item in allreport)
             {
-                return null;//currentModel.allReports();                
+                if (true)
+                {
+                    North.Add(item);
+                }
+                if (true)
+                {
+                    South.Add(item);
+                }
+                if (true)
+                {
+                    Center.Add(item);
+                }
+                else
+                {
+                    Other.Add(item);
+                }
             }
+
         }
     }
 }
